8a07ebd7c89210d6d964cdd79dd57eab230ff63b
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2025-06-06 00:16:52 +0530

    initial setup

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module shaadi-saga
+
+go 1.23.2
---
+++ backend/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// This line is not needed and will cause a compilation error
