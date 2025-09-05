a66cd6bc80602c76338e821a19f0dce29a0f65bd
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2025-09-05 14:02:39 +0530

    minor changes in login flow

--- frontend/my-app/src/routes/vendors/dashboard/+page.svelte
+++ frontend/my-app/src/routes/vendors/dashboard/+page.svelte
@@ -1,17 +1,3 @@
-<script>
-        export let data;
-        import { supabase } from '$lib/supabaseClient.js';
-        import { goto } from '$app/navigation';
-        async function SignOut(){
-            await supabase.auth.signOut()
-            location.href="/vendors"
-        }
-</script>
 <main>
-    {#if data.vendor}
-        <h2>Welcome {data.vendor.owner_name}</h2>
-    {:else}
-        <p>Error fetching the details!!!</p>
-    {/if}
-    <button on:click={SignOut}>logout!</button>
+    this is the dashboard
 </main>
